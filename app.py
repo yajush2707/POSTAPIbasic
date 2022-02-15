7607d7a230617f023169c6c7e04e528df06a9485
yajush2707 <95477320+yajush2707@users.noreply.github.com>
2022-02-15 18:29:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request 
 app = Flask(__name__)
 
-languages = [{'Unnamed' : 'Yajush'}, {'Unnamed: 0.1' : 'Chaudhary'} , {'BIC / BLZ,Beguenstigter / Auftraggeber': 'DEUTDEFFXXX'} , {'Betrag': 'XXXXeuros'} , {'Buchungstag': '15.02.2022'} ,{'Verwendungszweck': 'Testing'} ,{'Umsatzart,IBAN / Kontonummer': '0980xxxx'} ,{'content': '-Content value-' }]
+inputdata = [{'Unnamed' : 'Yajush'}, {'Unnamed: 0.1' : 'Chaudhary'} , {'BIC / BLZ,Beguenstigter / Auftraggeber': 'DEUTDEFFXXX'} , {'Betrag': 'XXXXeuros'} , {'Buchungstag': '15.02.2022'} ,{'Verwendungszweck': 'Testing'} ,{'Umsatzart,IBAN / Kontonummer': '0980xxxx'} ,{'content': '-Content value-' }]
 infile = open('input.txt', 'r')
 firstLine = infile.readline()
 
@@ -11,7 +11,7 @@ def test():
 
 @app.route('/Unnamed: 0,Unnamed: 0.1,BIC / BLZ,Beguenstigter / Auftraggeber,Betrag,Buchungstag,Verwendungszweck,Umsatzart,IBAN / Kontonummer,content', methods = ['GET'])
 def returnAll():
-    return jsonify (languages)
+    return jsonify (inputdata)
 
 if __name__ == '__main__': 
     app.run(debug=True, port =8080)
